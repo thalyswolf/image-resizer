2e6a47bf6745f0a316b15611cef8dfba972b7472
Thalys Wolf <thalyswolf01@gmail.com>
2021-12-02 22:50:23 -0300

    refactor: adjustment on usecase

--- api/src/core/usecase/to_queue_image_to_resize_usecase.py
+++ api/src/core/usecase/to_queue_image_to_resize_usecase.py
@@ -1,7 +1,26 @@
+from typing import Dict
+from src.contract.controller_contract import FilesRequest
+
+from src.core.entity.image import Image
+
 
 class ToQueueImageToResizeUseCase:
     def __init__(self, messaging_queue):
         self.messaging_queue = messaging_queue
 
-    def execute(self, image_request):
-        self.messaging_queue.send_to_queue()
+    def execute(self, request: Dict, file: FilesRequest):
+
+        image_to_resize = Image()
+        image_to_resize.name = file.name
+        image_to_resize.str_data = file.data
+
+        self.messaging_queue.send_to_resize(image_to_resize)
+
+
+
+
+
+        return {
+            'message': 'Sucesso'
+        }
+
