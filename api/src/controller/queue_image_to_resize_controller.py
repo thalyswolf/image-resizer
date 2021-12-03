90e516e593a6ce6719f2b0749c9074dfc83db1cc
Thalys Wolf <thalyswolf01@gmail.com>
2021-12-02 22:49:36 -0300

    refactor: adjustment on image controller contract for handler errors

--- api/src/controller/queue_image_to_resize_controller.py
+++ api/src/controller/queue_image_to_resize_controller.py
@@ -1,3 +1,6 @@
+from traceback import format_exc
+
+from src.helpers.handler_errors import GenericErrorException
 from src.core.usecase.to_queue_image_to_resize_usecase import ToQueueImageToResizeUseCase
 from src.contract.controller_contract import HttpRequest, HttpResponse
 from src.factory.messaging_queue_factory import get_queue_messaging_factory
@@ -5,7 +8,19 @@ from src.factory.messaging_queue_factory import get_queue_messaging_factory
 
 class QueueImageToResizeController:
 
-    def resize_image(self, request: HttpRequest) -> HttpResponse:
-        messaging_queue = get_queue_messaging_factory()
+    @staticmethod
+    def resize_image(request: HttpRequest) -> HttpResponse:
+        try:
+            messaging_queue = get_queue_messaging_factory()
 
-        image = ToQueueImageToResizeUseCase(messaging_queue).execute(request.payload)
+            _ = ToQueueImageToResizeUseCase(messaging_queue).execute(request.payload, request.files[0])
+
+            return HttpResponse(202, {
+                'message': 'Accepted, but processing ...'
+            })
+
+        except Exception:
+            return HttpResponse(500, {
+                'message': format_exc()
+            })
+
