44c78d4bfa4edf35846235acc8ee8aa85f4b724f
Thalys Wolf <thalyswolf01@gmail.com>
2021-12-02 23:04:23 -0300

    refactor

--- api/src/infra/queue/rabbitmq/rabbitmq.py
+++ api/src/infra/queue/rabbitmq/rabbitmq.py
@@ -1,21 +1,14 @@
+from src.infra.queue.rabbitmq.connection import RabbitConnection
 from src.contract.messaging_queue_contract import MessagingQueueContract
-
+from json import dumps
 
 class RabbitMQMessaging(MessagingQueueContract):
-    def send_to_queue(self):
-        print('send via rabbit')
+    def send_to_resize(self, image):
 
-        import pika
+        data = dumps({
+            'message': 'teste hahahaha',
+            'image': image.str_data
+        })
 
-        connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
-        channel = connection.channel()
-
-        channel.queue_declare(queue='hello')
-
-        channel.basic_publish(exchange='',
-                            routing_key='hello',
-                            body='Hello World!')
-
-        print(" [x] Sent 'Hello World!'")
-
-        connection.close()
+        RabbitConnection().send_message(data)
+        print('enviado')
