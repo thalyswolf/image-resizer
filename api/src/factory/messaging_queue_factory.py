16203de46d0db938b6aa04269f549fdc124bc3b1
Thalys Wolf <thalyswolf01@gmail.com>
2021-12-05 21:37:53 -0300

    refactor: removed unused import on factory

--- api/src/factory/messaging_queue_factory.py
+++ api/src/factory/messaging_queue_factory.py
@@ -1,6 +1,4 @@
-from src.infra.queue.local.local import LocalMessaging
 from src.infra.queue.rabbitmq.rabbitmq import RabbitMQMessaging
 
 def get_queue_messaging_factory():
     return RabbitMQMessaging()
-    return LocalMessaging()
