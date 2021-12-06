0fc1335b6f2f8a6eb29bd6624580b04c6f533c8d
Thalys Wolf <thalyswolf01@gmail.com>
2021-12-05 21:18:15 -0300

    chore: add rabbitmq adapter

---
+++ api/src/adapter/image_to_resize_rabbitmq_adapter.py
@@ -0,0 +1,15 @@
+from json import dumps
+
+from src.core.entity.image_to_resize import ImageToResize
+
+def image_to_resize_rabbitmq_adapter(image_resize: ImageToResize):
+    return dumps({
+        'body': {
+            'newWidth': image_resize.new_width,
+            'newHeight': image_resize.new_height
+        },
+        'file': {
+            'mime': image_resize.image.mime,
+            'file': image_resize.image.str_data
+        }
+    })
