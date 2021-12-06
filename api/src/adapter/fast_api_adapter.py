2a05f74603244bfbd1877e0e5462e1613d64b2bb
Thalys Wolf <thalyswolf01@gmail.com>
2021-12-05 21:17:55 -0300

    refactor: update fastapi adapter

--- api/src/adapter/fast_api_adapter.py
+++ api/src/adapter/fast_api_adapter.py
@@ -3,11 +3,17 @@ from typing import Dict
 from src.contract.controller_contract import HttpRequest, FilesRequest
 
 def fast_api_adapter(request: Dict) -> HttpRequest:
+
+    files = []
+
+    for file in request['files']:
+        files.append(FilesRequest(file.filename.split(".")[-1], str(file.file.read())))
+
     http_request = HttpRequest(
         header=request['headers'],
         payload=request['body'],
         params=request['query'],
-        files=request['files']
+        files=files
     )
 
     return http_request
