5bc2ca34b9a666a7ead3171e99d3843f409cdcf3
Thalys Wolf <thalyswolf01@gmail.com>
2021-12-06 21:54:15 -0300

    refactor: adjustment on usecase for to keep readable code

--- resizer/src/core/usecase/resize_image_usecase.py
+++ resizer/src/core/usecase/resize_image_usecase.py
@@ -1,7 +1,9 @@
 from typing import Dict
 
-from src.helpers.image_utils import get_image_from_string_byte, resize_image
+from src.helpers.image_utils import get_image_from_string_byte, resize_image, save_img_resize_on_dir
 from src.contract.controller_contract import FilesRequest
+from src.core.entity.image import Image
+from src.core.entity.image_to_resize import ImageToResize
 import cv2
 
 class ResizeImageUseCase:
@@ -11,15 +13,22 @@ class ResizeImageUseCase:
     def execute(self, request: Dict, file: FilesRequest):
 
         try:
-            height = int(request['newHeight'])
-            width = int(request['newWidth'])
+            image_object = Image()
+            image_object.mime = file.mime
+            image_object.str_data = file.data
+
+            image_to_resize = ImageToResize()
+            image_to_resize._id = request['_id']
+            image_to_resize.new_height = int(request['newHeight'])
+            image_to_resize.new_width = int(request['newWidth'])
+            image_to_resize.image = image_object
+
         except:
             raise Exception()
         else:
 
-            image_data = file.data
-            image = get_image_from_string_byte(image_data)
-            image_resized = resize_image(image, width, height)
-       
+            image = get_image_from_string_byte(image_to_resize)
+            image_resized = resize_image(image, image_to_resize.new_height, image_to_resize.new_width)
+            save_img_resize_on_dir(image_resized, image_to_resize)
+
             print('final')
-            cv2.imwrite('./src/storage/teste-agora.{}'.format(file.mime), image_resized)
