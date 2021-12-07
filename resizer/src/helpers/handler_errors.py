ad0b6d3d63491a14cb35b0a6ed0924991e82b29a
Thalys Wolf <thalyswolf01@gmail.com>
2021-12-07 08:38:52 -0300

    refactor: add more errors to handle

--- resizer/src/helpers/handler_errors.py
+++ resizer/src/helpers/handler_errors.py
@@ -5,3 +5,27 @@ class GenericErrorException(Exception):
     def __init__(self, message='An error occures'):
         self.message = message
         super().__init__(self.message)
+
+class InvalidHeightErrorException(Exception):
+
+    """ Exception raised when the height informed is invalid """
+
+    def __init__(self, message='the height is invalid'):
+        self.message = message
+        super().__init__(self.message)
+
+class InvalidWidthErrorException(Exception):
+
+    """ Exception raised when the width informed is invalid """
+
+    def __init__(self, message='the width is invalid'):
+        self.message = message
+        super().__init__(self.message)
+
+class InvalidTypeParamErrorException(Exception):
+
+    """ Exception raised when an param type is invalid """
+
+    def __init__(self, message='type is not valid'):
+        self.message = message
+        super().__init__(self.message)
