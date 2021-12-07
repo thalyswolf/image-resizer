94fdb26d6cd6e5c5c4b071fee4376cf69584087d
Thalys Wolf <thalyswolf01@gmail.com>
2021-12-06 21:51:56 -0300

    chore: add _id on adapter

--- api/src/adapter/image_to_resize_rabbitmq_adapter.py
+++ api/src/adapter/image_to_resize_rabbitmq_adapter.py
@@ -5,6 +5,7 @@ from src.core.entity.image_to_resize import ImageToResize
 def image_to_resize_rabbitmq_adapter(image_resize: ImageToResize):
     return dumps({
         'body': {
+            '_id': image_resize._id,
             'newWidth': image_resize.new_width,
             'newHeight': image_resize.new_height
         },
