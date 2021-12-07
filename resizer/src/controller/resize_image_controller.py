110dd22020a2f37f623492e54cf18a692a61b064
Thalys Wolf <thalyswolf01@gmail.com>
2021-12-07 08:36:58 -0300

    refactor: image-utils dependency injection

--- resizer/src/controller/resize_image_controller.py
+++ resizer/src/controller/resize_image_controller.py
@@ -1,3 +1,4 @@
+from src.factory.get_image_utils_factory import get_image_utils_factory
 from src.core.usecase.resize_image_usecase import ResizeImageUseCase
 from src.contract.controller_contract import MessagingRequest, MessagingResponse
 
@@ -7,7 +8,9 @@ class ResizeImageController:
     @staticmethod
     def resize_image(request: MessagingRequest) -> MessagingResponse:
         try:
-            ResizeImageUseCase().execute(request.payload, request.files[0])
+            image_utils = get_image_utils_factory()
+
+            ResizeImageUseCase(image_utils).execute(request.payload, request.files[0])
         
         except Exception:
             from traceback import format_exc
